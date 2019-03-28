15ff6cca138998a1d5837f631054744c1efef893
JEphron <jephron63@gmail.com>
2019-03-27 21:26:55 -0700

    Input caching

--- src/main/scala/util/Utils.scala
+++ src/main/scala/util/Utils.scala
@@ -1,4 +1,8 @@
 package util
+import java.io.{File, FileReader, FileWriter}
+import java.util.Scanner
+import java.util.prefs.Preferences
+
 import scalaj.http.Http
 
 import scala.io.Source
@@ -7,8 +11,21 @@ object Utils {
   val session: String = Source.fromResource("session.txt").getLines().mkString("\n")
 
   def getInput(year: Int, day: Int): String = {
-    val url = s"https://adventofcode.com/$year/day/$day/input"
-    Http(url).cookie("session", session).asString.body
+    val preferences = Preferences.userNodeForPackage(Utils.getClass)
+    val prefsKey = s"cached-advent-input-$year-$day"
+    val cacheFilePath = preferences.get(prefsKey, "")
+    if (cacheFilePath == "") {
+      val url = s"https://adventofcode.com/$year/day/$day/input"
+      val input = Http(url).cookie("session", session).asString.body
+      val file = File.createTempFile(prefsKey, "txt")
+      val writer = new FileWriter(file)
+      writer.write(input)
+      writer.close()
+      preferences.put(prefsKey, file.getAbsolutePath)
+      input
+    } else {
+      Source.fromFile(cacheFilePath).mkString
+    }
   }
 
   def count[T](seq : Seq[T]) : Map[T, Int] = countBy[T, T](identity)(seq)
